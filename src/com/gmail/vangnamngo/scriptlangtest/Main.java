fa40d8b2a3686302dbbdc291b486969198113bf1
Vang Ngo <vang.nam.ngo@gmail.com>
2022-03-21 16:06:03 -0400

    Add missing semicolon

--- src/com/gmail/vangnamngo/scriptlangtest/Main.java
+++ src/com/gmail/vangnamngo/scriptlangtest/Main.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class Main {
 
     public final static AbstractObject NULL_OBJ = new AbstractObject() {
-    }
+    };
 
     public static void main(String args[]) {
         File thisDir = new File(".");
